6a2cef522c5495e88a4d9121f219171c8c0eb2b0
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-01-31 21:29:27 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,51 @@
+# Pacman Tools
+
+This is a simple set of tools for pacman, the package manager for Arch Linux-based distributions. It can clean the pacman cache, remove unused packages, fix pacman keys and update pacman mirrors.
+
+## Installation
+
+To install pacman tools, clone this repository and run the install script:
+
+```shell
+git clone https://github.com/Angel-Karasu/pacman-tools.git;
+cd pacman-tools;
+chmod +x ./install.sh;
+./install.sh
+```
+
+## Usage
+To get the list of commands, run :
+- For pacman cleaner :
+```shell
+clean-pacman -h
+```
+- For fix the keys
+```shell
+fix-keys -h
+```
+- To update the mirrors
+```shell
+update-mirrors -h
+```
+
+## Update
+
+To update pacman-tools, run :
+```shell
+update-pacman-tools
+```
+
+## Uninstall
+
+To uninstall pacman-tools, run :
+```shell
+/etc/pacman.d/pacman-tools/uninstall.sh
+```
+
+## License
+
+This project is licensed under the [GNU GPLv3](https://choosealicense.com/licenses/gpl-3.0/). See the `LICENSE` file for details.
+
+## Contributing
+
+Contributions are welcome. Please open an issue or a pull request if you want to help improve the project.
--- install.sh
+++ install.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
 
-BASE_DIR=$(realpath `dirname $0`)
+cd $(realpath `dirname $0`)
+source pacman-tools/check_status.sh
 
-if sudo -v; then
-    echo -e "Install pacman-tools\n"
+check_sudo
 
-    echo "Add the scripts in /etc/pacman.d/pacman-tools/"
-    sudo cp -r $BASE_DIR/pacman-tools /etc/pacman.d/
-    sudo chmod -R +x /etc/pacman.d/pacman-tools/
+echo -e "Install pacman-tools\n"
 
-    echo "Add aliases for bash"
-    sudo cp $BASE_DIR/pacman_tools.bashrc /etc/bash/bashrc.d/
-
-    if [[ ! $(pacman -Qi curl git pacman-contrib sed >&/dev/null; echo $?) ]]; then
-        if [[ $(ping -c 1 -q github.com >&/dev/null; echo $?) ]]; then
-            echo "Install packages required"
-            sudo pacman -S --needed --noconfirm curl git pacman-contrib sed 2>/dev/null
-        else echo "Before to use, you must have curl, git, pacman-contrib and sed installed"; fi
-    fi
-
-    echo -e "\nSuccess to install pacman-tools"
-    exit 0
-else
-    echo "Root privileges denied"
-    exit 1
+if [ $(pacman -Qi curl git pacman-contrib sed >&/dev/null; echo $?) != 0 ]; then
+    check_internet
+    
+    echo "Install packages required"
+    sudo pacman -S --needed --noconfirm curl git pacman-contrib sed 2>/dev/null
 fi
+
+echo "Add the scripts in /etc/pacman.d/pacman-tools/"
+sudo cp -r ./pacman-tools /etc/pacman.d/
+sudo chmod -R +x /etc/pacman.d/pacman-tools/
+
+echo "Add aliases for bash"
+sudo cp ./pacman_tools.bashrc /etc/bash/bashrc.d/
+
+echo -e "\nSuccess to install pacman-tools"
+exit 0
