6699912ef6b559aa44338091600fdbd7be7d8f59
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-02-05 19:19:24 +0100

    Repare

--- update_mirrors.sh
+++ update_mirrors.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 update_mirror_list() {
-    for server in `curl -s $1 | sed -e "/Server/b" -e d | tr -d '# '`; do
-        t=`ping -c 1 $(echo $server | sed "s|.*//||; s|/.*||") 2>/dev/null | tail -1 | cut -d '/' -f 5`
+    for server in `curl -s "$1" | sed -e "/Server/b" -e d | tr -d '# '`; do
+        t=`ping -c 1 "$(echo $server | sed 's|.*//||; s|/.*||')" 2>/dev/null | tail -1 | cut -d '/' -f 5`
         echo `echo $server | sed "s|.*=||"`"  **$t**"
         [ "$t" ] && list+=" $t,$server"
     done
 
-    echo "$list" | tr ' ' '\n' | sort -t',' -k1,1n | sed "s|.*,||; s|=| = |g" | sudo tee /etc/pacman.d/$2
+    echo "$list" | tr ' ' '\n' | sort -t',' -k1,1n | sed 's|.*,||; s|=| = |g' | sudo tee /etc/pacman.d/$2
 
     printf "\nSuccess to update mirrors\n\n"
 }
