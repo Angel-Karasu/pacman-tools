97d5befd82d0804ca198b94b3b96120c47eeb09b
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-02-06 20:01:30 +0100

    Auto-detect chaotic-mirrorlist

--- add_update_mirrors_commands.sh
+++ add_update_mirrors_commands.sh
@@ -10,8 +10,10 @@ add_in_update_mirrrors() { add_in_pactools "update_mirror_list '$1' '$2' '$3'"; 
 
 add_arch() { add_in_update_mirrrors "https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on" mirrorlist$1; }
 
+[ "`pacman -T chaotic-mirrorlist`" ] || add_in_update_mirrrors "https://aur.chaotic.cx/mirrorlist.txt" chaotic-mirrorlist;
+
 case $ID in
-    arch)
+    arch|garuda)
         add_arch;;
     artix)
         add_in_update_mirrrors "https://gitea.artixlinux.org/packages/artix-mirrorlist/raw/branch/master/mirrorlist" mirrorlist
@@ -19,10 +21,6 @@ case $ID in
         ;;
     endeavouros)
         add_in_update_mirrrors "https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist" endeavouros-mirrorlist
-        add_arch
-        ;;
-    garuda)
-        add_in_update_mirrrors "https://aur.chaotic.cx/mirrorlist.txt" chaotic-mirrorlist
         add_arch
         ;;
     manjaro)
