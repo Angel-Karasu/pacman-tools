f8b83d63e08ae7e94725ec057c5b8e2af3881fe7
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-02-10 12:17:36 +0100

    Update EndeavourOS

--- add_update_mirrors_commands.sh
+++ add_update_mirrors_commands.sh
@@ -21,13 +21,12 @@ case $ID in
         ;;
     blackarch|athena)
         add_distrib_and_arch "https://raw.githubusercontent.com/BlackArch/blackarch-site/master/blackarch-mirrorlist" blackarch;;
-    endeavouros)
-        add_distrib_and_arch "https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist" endeavouros;;
 esac
 
 # Sort by number of https server
 [ "`pacman -T rebornos-mirrorlist`" ] || add_in_update_mirrrors "https://raw.githubusercontent.com/RebornOS-Team/rebornos-mirrorlist/main/reborn-mirrorlist" reborn-;
 [ "`pacman -T arcolinux-mirrorlist-git`" ] || add_in_update_mirrrors "https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist" arcolinux-;
+[ "`pacman -T endeavouros-mirrorlist`" ] || add_in_update_mirrrors "https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist" endeavouros-;
 [ "`pacman -T chaotic-mirrorlist`" ] || add_in_update_mirrrors "https://aur.chaotic.cx/mirrorlist.txt" chaotic-;
 [ "`pacman -T pacman-mirrors`" ] || add_in_update_mirrrors "https://repo.manjaro.org/mirrors.json" '' 'stable/$repo/$arch';
 [ "`pacman -T pacman-mirrorlist`" ] || add_arch;
