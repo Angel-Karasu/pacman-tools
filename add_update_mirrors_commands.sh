3f5549abf79e2c97c6a8901e352c258e6b5a6fb1
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-02-10 12:55:57 +0100

    Update

--- add_update_mirrors_commands.sh
+++ add_update_mirrors_commands.sh
@@ -6,24 +6,26 @@
 sudo sed -i '/# Start commands/,/# End commands/{//!d}' ./pactools.sh
 add_in_pactools () { sudo sed -i "/# End commands/i\ \t$1" ./pactools.sh; }
 
-add_in_update_mirrrors() { add_in_pactools "update_mirror_list '$1' '$2mirrorlist' '$3'"; }
+add_in_update_mirrrors() { add_in_pactools "update_mirror_list '$1' '$2' '$3'"; }
 
-add_arch() { add_in_update_mirrrors "https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on"; }
+add_mirrorlist() { [ "`pacman -T $1`" ] || add_in_update_mirrrors $2 `[ "$3" ] && echo $3 || echo $1` $4; }
+
+add_arch_mirrorlist() { add_mirrorlist 'pacman-mirrorlist' 'https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on' "mirrorlist$2"; }
 
 case $ID in
     artix)
         add_in_update_mirrrors "https://gitea.artixlinux.org/packages/artix-mirrorlist/raw/branch/master/mirrorlist" mirrorlist
-        [ "`pacman -T archlinux-mirrorlist`" ] || add_arch -arch
+        [ "`pacman -T archlinux-mirrorlist`" ] || add_arch_mirrorlist -arch
         ;;
 esac
 
 # Sort by number of https server
-[ "`pacman -T rebornos-mirrorlist`" ] || add_in_update_mirrrors "https://raw.githubusercontent.com/RebornOS-Team/rebornos-mirrorlist/main/reborn-mirrorlist" reborn-;
-[ "`pacman -T arcolinux-mirrorlist-git`" ] || add_in_update_mirrrors "https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist" arcolinux-;
-[ "`pacman -T endeavouros-mirrorlist`" ] || add_in_update_mirrrors "https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist" endeavouros-;
-[ "`pacman -T chaotic-mirrorlist`" ] || add_in_update_mirrrors "https://aur.chaotic.cx/mirrorlist.txt" chaotic-;
-[ "`pacman -T blackarch-mirrorlist`" ] || add_in_update_mirrrors "https://raw.githubusercontent.com/BlackArch/blackarch-site/master/blackarch-mirrorlist" blackarch-;
-[ "`pacman -T pacman-mirrors`" ] || add_in_update_mirrrors "https://repo.manjaro.org/mirrors.json" '' 'stable/$repo/$arch';
-[ "`pacman -T pacman-mirrorlist`" ] || add_arch;
+add_mirrorlist 'rebornos-mirrorlist' 'https://raw.githubusercontent.com/RebornOS-Team/rebornos-mirrorlist/main/reborn-mirrorlist' 'reborn-mirrorlist';
+add_mirrorlist 'arcolinux-mirrorlist-git' 'https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist' 'arcolinux-mirrorlist';
+add_mirrorlist 'endeavouros-mirrorlist' 'https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist';
+add_mirrorlist 'chaotic-mirrorlist' 'https://aur.chaotic.cx/mirrorlist.txt';
+add_mirrorlist 'blackarch-mirrorlist' 'https://raw.githubusercontent.com/BlackArch/blackarch-site/master/blackarch-mirrorlist';
+add_mirrorlist 'pacman-mirrors' 'https://repo.manjaro.org/mirrors.json' 'mirrorlist' 'stable/$repo/$arch';
+add_arch_mirrorlist;
 
 add_in_pactools 'sudo pacman -Syy'
