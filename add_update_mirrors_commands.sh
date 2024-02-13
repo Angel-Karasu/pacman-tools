9b84055f58522468c6be3fa40f7eff1855255277
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-02-13 22:26:41 +0100

    Update manjaro

--- add_update_mirrors_commands.sh
+++ add_update_mirrors_commands.sh
@@ -10,7 +10,7 @@ add_in_update_mirrrors() { add_in_pactools "update_mirror_list '$1' '$2' '$3'"; 
 add_mirrorlist() { [ "`pacman -T $1`" ] || add_in_update_mirrrors $2 `[ "$3" ] && echo $3 || echo $1` $4; }
 
 add_arch_mirrorlist() { add_mirrorlist "$1" 'https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on' "$2mirrorlist"; }
-add_pacman_mirrorlist() { add_mirrorlist 'pacman-mirrorlist' $1 'mirrorlist'; }
+add_pacman_mirrorlist() { add_mirrorlist 'pacman-mirrorlist' $1 'mirrorlist' $2; }
 
 # Sort by number of https server
 add_mirrorlist 'rebornos-mirrorlist' 'https://raw.githubusercontent.com/RebornOS-Team/rebornos-mirrorlist/main/reborn-mirrorlist' 'reborn-mirrorlist';
@@ -19,11 +19,11 @@ add_mirrorlist 'arcolinux-mirrorlist-git' 'https://raw.githubusercontent.com/arc
 add_mirrorlist 'endeavouros-mirrorlist' 'https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist';
 add_mirrorlist 'chaotic-mirrorlist' 'https://aur.chaotic.cx/mirrorlist.txt';
 add_mirrorlist 'blackarch-mirrorlist' 'https://raw.githubusercontent.com/BlackArch/blackarch-site/master/blackarch-mirrorlist';
-add_mirrorlist 'pacman-mirrors' 'https://repo.manjaro.org/mirrors.json' 'mirrorlist' 'stable/$repo/$arch';
 case $ID in
     artix) add_pacman_mirrorlist 'https://gitea.artixlinux.org/packages/artix-mirrorlist/raw/branch/master/mirrorlist';;
-    kaos) add_pacman_mirrorlist 'https://raw.githubusercontent.com/KaOSx/core/master/pacman-mirrorlist/mirrorlist';;
     hyperbola) add_pacman_mirrorlist 'https://www.hyperbola.info/mirrorlist/?country=all&protocol=https&use_mirror_status=on';;
+    kaos) add_pacman_mirrorlist 'https://raw.githubusercontent.com/KaOSx/core/master/pacman-mirrorlist/mirrorlist';;
+    manjaro) add_pacman_mirrorlist 'https://repo.manjaro.org/mirrors.json' 'stable/$repo/$arch';;
     parabola) add_pacman_mirrorlist 'https://www.parabola.nu/mirrorlist/?country=all&protocol=https&use_mirror_status=on';;
     *) add_arch_mirrorlist 'pacman-mirrorlist';;
 esac
