fdd1c154189aae02400970e546559576a1d91737
Angel <157843979+Angel-Karasu@users.noreply.github.com>
2024-05-27 19:54:26 +0200

    Update add_update_mirrors_commands.sh
    Add cachyos v3 and v4

--- add_update_mirrors_commands.sh
+++ add_update_mirrors_commands.sh
@@ -10,10 +10,11 @@ add_in_update_mirrrors() { add_in_pactools "update_mirror_list '$1' '$2' '$3' '$
 add_mirrorlist() { [ "`pacman -T $1`" ] || add_in_update_mirrrors $2 $3 `[ "$4" ] && echo $4 || echo $1` $5; }
 
 add_arch_mirrorlist() { add_mirrorlist "$1" "https://archlinux.org/mirrorlist/?country=all&protocol=https&use_mirror_status=on" core "$2mirrorlist"; }
+add_cachyos_mirrorlist() { add_mirrorlist 'cachyos$1-mirrorlist' 'https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/cachyos$1-mirrorlist/cachyos$1-mirrorlist' cachyos$1; }
 add_pacman_mirrorlist() { add_mirrorlist 'pacman-mirrorlist' $1 $2 'mirrorlist' $3; }
 
 add_mirrorlist 'rebornos-mirrorlist' 'https://raw.githubusercontent.com/RebornOS-Team/rebornos-mirrorlist/main/reborn-mirrorlist' 'Reborn-OS' 'reborn-mirrorlist';
-add_mirrorlist 'cachyos-mirrorlist' 'https://raw.githubusercontent.com/CachyOS/CachyOS-PKGBUILDS/master/cachyos-mirrorlist/cachyos-mirrorlist' cachyos;
+for v in "" "-v3" "-v4"; do add_cachyos_mirrorlist $v; done;
 add_mirrorlist 'arcolinux-mirrorlist-git' 'https://raw.githubusercontent.com/arcolinux/arcolinux-mirrorlist/master/etc/pacman.d/arcolinux-mirrorlist' 'arcolinux_repo' 'arcolinux-mirrorlist';
 add_mirrorlist 'endeavouros-mirrorlist' 'https://gitlab.com/endeavouros-filemirror/PKGBUILDS/-/raw/master/endeavouros-mirrorlist/endeavouros-mirrorlist' endeavouros;
 add_mirrorlist 'blackarch-mirrorlist' 'https://raw.githubusercontent.com/BlackArch/blackarch-site/master/blackarch-mirrorlist' blackarch;
